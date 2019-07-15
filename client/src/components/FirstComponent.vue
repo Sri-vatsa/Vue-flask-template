8c3be45c63bef2e2e2cbaad15f93356a21b4c670
Srivatsa <p.psrivatsa@gmail.com>
2019-07-15 23:59:37 +0200

    Create project template

---
+++ client/src/components/FirstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'first_comp',
+  data() {
+    return {
+      msg: 'Hello!',
+    };
+  },
+};
+</script>
